8c4a4721ecb105d040c9d16d4b3136a48a4612b7
shahir29 <shahir29ams@gmail.com>
2025-09-20 08:36:57 -0700

    change the java code

--- java-docker-app/src/main/java/com/example/demo/Application.java
+++ java-docker-app/src/main/java/com/example/demo/Application.java
@@ -1,19 +1,11 @@
-package com.example.demo;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
-@RestController
-public class Application {   
-
-    @GetMapping("/")
-    public String home() {
-        return "Hello from Jenkins + Java + Docker!";
-    }
-
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args); // 👈 also updated here
+        SpringApplication.run(Application.class, args);
     }
 }
