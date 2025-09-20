2414633b9a15cfd2acd57aba399af45c63e3c45d
shahir29 <shahir29ams@gmail.com>
2025-09-20 10:14:12 -0700

    add the folder

--- java-docker-app/src/main/java/com/example/Application.java
+++ java-docker-app/src/main/java/com/example/Application.java
@@ -7,15 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-}
 
-@RestController
-class HelloController {
-    @GetMapping("/")
-    public String hello() {
-        return "Hello from Spring Boot running via Jenkins!";
+    @RestController
+    class HelloController {
+        @GetMapping("/")
+        public String home() {
+            return "Hello from Jenkins-deployed Spring Boot app!";
+        }
     }
 }
